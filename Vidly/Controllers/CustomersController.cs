321dfcae6ede9a8df92bf42d44238a0c3491fa95
stenak <s.nakevski@live.com>
2017-11-18 16:59:01 +0100

    InitialMigration

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -28,8 +28,12 @@ namespace Vidly.Controllers
         {
             return new List<Customer>
             {
-                new Customer { Id = 1, Name = "John Smith" },
-                new Customer { Id = 2, Name = "Mary Williams" }
+                new Customer { Id = 1, Name = "John Smith", BirthYear = 1988 },
+                new Customer { Id = 2, Name = "Boris Petrusevski", BirthYear = 2010 },
+                new Customer { Id = 3, Name = "Kire Nakevski", BirthYear = 1995 },
+                new Customer { Id = 4, Name = "Valentina Dimovska", BirthYear = 1988 },
+                new Customer { Id = 5, Name = "Steve Nakevski", BirthYear = 1989 },
+                new Customer { Id = 6, Name = "Mary Williams", BirthYear = 1972 }
             };
         }
     }
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -11,5 +11,4 @@ namespace Vidly.Models
         public string Name { get; set; }
     }
 
-    //movies/random
 }
